0f419eeea81574e4689577d2a8dae54cc63bb823
Mike Sperber <sperber@deinprogramm.de>
2026-06-29 13:31:58 +0200

    Support more systems in Nix flake

--- flake.nix
+++ flake.nix
@@ -3,37 +3,55 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
 
     testAuditor = {
-      url = "github:Zip-creations/testAuditor";
+      url = "github:mikesperber/testAuditor?ref=moreSystems";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, testAuditor }:
-    let
-      system = "x86_64-linux";
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      testAuditor,
+      flake-parts,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+        "aarch64-linux"
+      ];
 
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
-    in {
-      packages.${system} = {
-        hello = pkgs.hello;
-        default = pkgs.hello;
-      };
+      perSystem =
+        { system, self', ... }:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
+        in
+        {
+          packages = {
+            hello = pkgs.hello;
+            default = pkgs.hello;
+          };
 
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [
-          (pkgs.python3.withPackages (ps: [
-            ps.pytest
-            ps.pytest-tap
-            ps.pytest-json-report
-          ]))
+          devShells.default = pkgs.mkShell {
+            buildInputs = [
+              (pkgs.python3.withPackages (ps: [
+                ps.pytest
+                ps.pytest-tap
+                ps.pytest-json-report
+              ]))
 
-          testAuditor.packages.${system}.default
-        ];
-      };
+              testAuditor.packages.${system}.default
+            ];
+          };
+        };
     };
 }
